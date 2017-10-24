bb86b105e6fa9303595e7a68903c581d7732b42e
Willem Koopman <willem.koopman@ft.com>
2017-10-24 14:41:24 +0100

    YASSS

--- ratemet.py
+++ ratemet.py
@@ -1,16 +1,26 @@
 from flask import Flask, request
 from flask_restful import Resource, Api
 from senti_classifier import senti_classifier
+import json
 
 app = Flask(__name__)
 api = Api(app)
 
 class returnSentiment(Resource):
     def get(self):
-        return {"Error":"Please post a string of data"}
+        return """
+        Please post a json string in the following way:
+        {"sentences":["on a long and frosty morning.","I asid hello"]}
+        """
     def post(self):
-        gloop = request.form['data']
-        sentences = gloop.split('.')
+        json_data = request.get_json(force=True)
+        #gloop = request.form['data']
+        #sentences = gloop.split('.')
+        print json_data
+        if 'sentences' not in json_data:
+            return "Nope"
+        sentences = json_data['sentences']
+
 
         pos_score, neg_score = senti_classifier.polarity_scores(sentences)
         print sentences
@@ -20,4 +30,5 @@ class returnSentiment(Resource):
 
 api.add_resource(returnSentiment, '/')
 if __name__ == '__main__':
-    app.run(host="0.0.0.0", port=80,debug=True)
+    #app.run(host="0.0.0.0", port=80,debug=True)
+    app.run(debug=True)
